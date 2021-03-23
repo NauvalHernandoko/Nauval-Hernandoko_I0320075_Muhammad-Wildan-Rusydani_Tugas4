9c9a503266af0d17e5b024c55066660dbce6dbdf
Nauval Hernandoko <nauvalhernadoko@gmail.com>
2021-03-23 16:27:54 +0700

    Exercise 4.1 Tugas 4

---
+++ I0320075_Excercise4.1.py
@@ -0,0 +1,20 @@
+x = 21
+y = 7
+
+# output : x + y = 28
+print('x + y = ',x + y )
+
+# output : x - y = 14
+print('x - y = ',x - y)
+
+# output : x * y = 147
+print('x * y = ',x * y)
+
+# output : x / y = 3
+print('x / y = ',x / y)
+
+# output : x ** y = 1801088541
+print('x ** y = ',x ** y)
+
+# output : x + y = 3
+print('x // y = ',x // y)
