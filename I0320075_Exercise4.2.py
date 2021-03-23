a6d52577919e03d0d6426ee85cee9cefa7f91b3f
Nauval Hernandoko <nauvalhernadoko@gmail.com>
2021-03-23 16:35:57 +0700

    Exercise 4.2 Tugas 4

---
+++ I0320075_Exercise4.2.py
@@ -0,0 +1,21 @@
+x = 50
+x = 47
+y = 49
+
+# output: x>y is false
+print('x > y is',x > y)
+
+# output: x<y is True
+print('x < y is',x < y)
+
+# output: x==y is false
+print('x == y is',x == y)
+
+# output: x!=y is true
+print('x != y is',x != y)
+
+# output: x>=y is false
+print('x >= y is',x >= y)
+
+# output: x<=y is false
+print('x <= y is',x <= y)
