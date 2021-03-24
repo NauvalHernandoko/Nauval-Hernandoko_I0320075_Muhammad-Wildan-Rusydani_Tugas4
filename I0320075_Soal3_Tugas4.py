e59aaf35594ffaa26b0b57d4251b7553e6a6c440
Nauval Hernandoko <nauvalhernadoko@gmail.com>
2021-03-24 20:29:23 +0700

    Tugas 4

--- I0320075_Soal3_Tugas4.py
+++ I0320075_Soal3_Tugas4.py
@@ -1,8 +1,14 @@
 #50 lbs = 22.73 kg
+print("=======memasukkan berat======")
+maksimal = 22.73
 masukkan_berat = float(input("Masukkan berat barang <= 22.73:"))
 print(masukkan_berat <= 22.73)
 
+print("=========sesuai soal=========")
 # a. Berat lebih 110 kg #false
+beratA = 111
+print('beratA <= maksimal is',beratA <= maksimal)
 # b. Berat 49 kg        #false
-
+beratB = 49
+print('beratB <= maksimal is',beratB <= maksimal)
 
